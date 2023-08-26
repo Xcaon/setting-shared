bcc11eae9c8f76552eb15fe7460604a6f15ac82c
Xcaon <fernandosteam@hotmail.es>
2023-08-26 15:39:19 +0200

    Dialog

--- AplicacionListaDeTareas/app/src/main/java/com/fernando/aplicacionlistadetareas/MainActivity.kt
+++ AplicacionListaDeTareas/app/src/main/java/com/fernando/aplicacionlistadetareas/MainActivity.kt
@@ -1,9 +1,15 @@
 package com.fernando.aplicacionlistadetareas
 
+import android.app.Dialog
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.EditText
+import android.widget.RadioButton
+import android.widget.RadioGroup
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 
 class MainActivity : AppCompatActivity() {
 
@@ -25,11 +31,62 @@ class MainActivity : AppCompatActivity() {
 
     private lateinit var rvTasks:RecyclerView
     private lateinit var tasksAdapter: TasksAdapter
+
+    private lateinit var fabAddTask: FloatingActionButton
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         initComponent()
         initUI()
+        initListeners()
+    }
+
+    private fun initListeners() {
+        fabAddTask.setOnClickListener {
+            showDialog()
+        }
+
+    }
+
+    private fun showDialog() {
+        val dialog = Dialog(this)
+        dialog.setContentView(R.layout.dialog_task)
+        dialog.show()
+        // Declaramos el boton del dialogo
+        val etTask = dialog.findViewById<EditText>(R.id.etTask)
+        val btAddTask = dialog.findViewById<Button>(R.id.btAddTask)
+        val rgTask = dialog.findViewById<RadioGroup>(R.id.rgTask)
+
+        btAddTask.setOnClickListener {
+
+            val currentTask = etTask.text.toString()
+
+            if ( currentTask.isNotEmpty()) {
+
+                // Nos dara el id del RabioButton seleccinado
+                val selectId = rgTask.checkedRadioButtonId
+                // Buscamos el id seleccionado y lo asignamos a un boton
+                val selectedRadioButton = rgTask.findViewById<RadioButton>(selectId)
+                // Recoger el texto que contenga ese RadioButton
+                // Tambien en vez de string podemos coger desde el xml de string con getString(R.string."nombre del string")
+                val currentCategory: TaskCategory = when (selectedRadioButton.text) {
+                    "Negocios" -> TaskCategory.Business
+                    "Personal" -> TaskCategory.Personal
+                    else -> TaskCategory.Other
+                }
+
+                // Añadir la informacion al array de objetos "tasks"
+                tasks.add(Task(currentTask, currentCategory))
+                // Avisamos al adapter de que hemos actualizado los datos
+                updateTasks()
+
+                // Escondemos el dialog
+                dialog.hide()
+            }
+        }
+
     }
 
     private fun initUI() {
@@ -47,6 +104,12 @@ class MainActivity : AppCompatActivity() {
     private fun initComponent() {
         rvCategories = findViewById(R.id.rvCategories)
         rvTasks = findViewById(R.id.rvTasks)
+        fabAddTask = findViewById(R.id.favAddTask)
+
+    }
+
+    private fun updateTasks(){
+        tasksAdapter.notifyDataSetChanged()
     }
 
 
