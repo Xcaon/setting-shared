d50d7e4cb58d421cdaf2d2edd130334cf3c0d4ff
Xcaon <fernandosteam@hotmail.es>
2023-09-24 18:42:09 +0200

    data store comenzando

---
+++ aplicacionSettings/app/src/main/java/com/fernando/aplicacionsettings/MainActivity.kt
@@ -0,0 +1,23 @@
+package com.fernando.aplicacionsettings
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.datastore.core.DataStore
+import androidx.datastore.preferences.core.Preferences
+import com.fernando.aplicacionsettings.databinding.ActivityMainBinding
+
+
+private val Context.dataStore : DataStore<Preferences>
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+    }
+}
